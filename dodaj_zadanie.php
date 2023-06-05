5e1acf54f6cb7d87dc9abbe3c8ff62d2699b91a0
Jakub Kittel <39377508+MidnightKyuba@users.noreply.github.com>
2023-06-05 09:46:12 +0200

    Add files via upload

---
+++ dodaj_zadanie.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dodaj zadanie</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> <!-- Dodaj bibliotekę ikon Font Awesome -->
+</head>
+<body>
+<header>
+    <div class="container-menu">
+        <div class="logo">
+            <a href="index.php"><img src="logo.jpg"></a>
+        </div>
+        <nav>
+            <ul class="menu">
+                <li><a href="index.php"><i class="fas fa-home"></i> Strona Główna</a></li>
+                <?php
+                session_start();
+                if (isset($_SESSION['username'])) {
+                    echo '<li><a href="lista_zadan.php"><i class="fas fa-tasks"></i> Lista Zadań</a></li>';
+                    if ($_SESSION['username'] === 'admin') {
+                        echo '<li><a href="admin.php"><i class="fas fa-cogs"></i> Panel Administracyjny</a></li>';
+                        echo '<li><a href="profil.php"><i class="fas fa-user-shield"></i> Admin</a></li>';
+                    } else {
+                        echo '<li><a href="konto.php"><i class="fas fa-user"></i> ' . $_SESSION['username'] . '</a></li>';
+                    }
+                } else {
+                    echo '<li><a href="login.php"><i class="fas fa-user"></i> Konto</a></li>';
+                }
+                ?>
+            </ul>
+        </nav>
+        <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+</header>
+
+<main>
+    <?php
+        if (isset($_SESSION['username'])) {
+            $username = $_SESSION['username'];
+
+            // Połączenie z bazą danych
+            $servername = "localhost";
+            $db_username = "root";
+            $db_password = "";
+            $dbname = "listazadan";
+            $conn = mysqli_connect($servername, $db_username, $db_password, $dbname);
+
+            // Sprawdzenie połączenia
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            if (isset($_POST['add_task'])) {
+                $userId = $_SESSION['userId'];
+                $title = $_POST['task_title'];
+                $description = $_POST['task_description'];
+                $status = $_POST['task_status'];
+                $endDate = $_POST['task_date'];
+                $priorityLevel = $_POST['task_priorityLevel'];
+                $forWho = $_POST['task_for'];
+                $groupId = 0;
+                if($forWho === 'group'){
+                    $groupId = $_POST['task_groupId'];
+                }
+
+                // Dodaj zadania do bazy danych
+                if($forWho === 'group'){
+                    $sql = "INSERT INTO tasks (title, description, status, endDate, priorityLevel, userId, groupId) VALUES ('$title', '$description', $status, '$endDate', $priorityLevel, null, $groupId)";
+                    mysqli_query($conn, $sql);
+                }
+                else{
+                    $sql = "INSERT INTO tasks (title, description, status, endDate, priorityLevel, userId, groupId) VALUES ('$title', '$description', $status, '$endDate', $priorityLevel, $userId, null)";
+                    mysqli_query($conn, $sql);
+                }
+            
+                // Przekierowanie po dodaniu zadania
+                header("Location: lista_zadan.php");
+                exit;
+            }
+
+            //Wyświetlenie formularza dodania zadania
+            $statusName = ['Nie Rozpoczęte','W Trakcie', 'Zakończone'];
+            $priorityName = ['Niski','Średni','Wysoki'];
+            echo '<form action="dodaj_zadanie.php" method="POST">';
+                echo '<label>Podaj tytuł zadania:</label>';
+                echo '<input class="add-user" type="text" name="task_title" required>';
+                echo '<label>Podaj opis zadania:</label>';
+                echo '<input class="add-user" type="text" name="task_description">';
+                echo '<label>Wybierz status zadania:</label>';
+                echo '<select name="task_status">';
+                for($i=0;$i<count($statusName);$i++){
+                    echo '<option value="'.$i.'">'.$statusName[$i].'</option>';
+                }
+                echo '</select>';
+                echo '<label>Podaj date do wykonania zadania:</label>';
+                echo '<input class="add-user" type="date" name="task_date">';
+                echo '<label>Wybierz priorytet:</label>';
+                echo '<select name="task_priorityLevel">';
+                    for($i=0;$i<count($priorityName);$i++){
+                        echo '<option value="'.$i.'">'.$priorityName[$i].'</option>';
+                    }
+                echo '</select>';
+                echo '<label>Dla kogo:</label>';
+                echo '<label><input type="radio" name="task_for" value="group">Grupy</label>';
+                echo '<label><input type="radio" name="task_for" value="me">Mnie</label>';
+                echo '<label>Wybierz grupę (jeśli wyżej wybrałeś opcje grupy):</label>';
+                echo '<select name="task_groupId">';
+                    $userId = $_SESSION['userId'];
+                    $sql = "SELECT groups.id as id, groups.name as name FROM groups INNER JOIN usersgroups ON usersgroups.groupId = groups.id WHERE usersgroups.userId=$userId";
+                    $result = mysqli_query($conn,$sql);
+                    while($row = mysqli_fetch_assoc($result)){
+                        echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+                    }
+                echo '</select>';
+                echo '<button class="btn" type="submit" name="add_task">Dodaj</button>';
+            echo'</form>';
+            mysqli_close($conn);
+        } else {
+            echo "<p>Nie jesteś zalogowany.</p>";
+        }
+    ?>
+</main>
+
+    <footer>
+    <div class="container-footer">
+        <div class="footer-content">
+        <div class="footer-info">
+            <h3>Lista Zadań</h3>
+            <p>Zorganizuj swoje zadania i osiągnij sukces!</p>
+            <p>Twórz listy zadań, śledź postępy i efektywnie zarządzaj czasem.</p>
+            <p>Kontakt: info@przykladowa-strona-zadan.pl</p>
+            <p>Telefon: 123-456-789</p>
+        </div>
+        <div class="footer-icons">
+            <a href="https://www.facebook.com"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.twitter.com"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.linkedin.com"><i class="fab fa-linkedin"></i></a>
+            <a href="https://www.youtube.com"><i class="fab fa-youtube"></i></a>
+        </div>
+        </div>
+    </div>
+</footer>
+
+    <div class="container-footer">
+        <p>Strona "Lista Zadań" Eliza Kozieł, Jakub Lewandowski, Jakub Kittel, Hubert Hejnowicz &copy; 2023</p>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
